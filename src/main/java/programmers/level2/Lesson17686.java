ad47eeb5a1943434da555db51fa8ff823b816060
KoreanShortHair <korshorthaircat@gmail.com>
2023-08-22 17:36:02 +0900

    [프로그래머스] 코딩테스트 연습 > 2018 KAKAO BLIND RECRUITMENT > [3차] 파일명 정렬

---
+++ src/main/java/programmers/level2/Lesson17686.java
@@ -0,0 +1,73 @@
+package programmers.level2;
+
+import java.util.*;
+
+/**
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/17686">[3차] 파일명 정렬</a>
+ */
+public class Lesson17686 {
+    public static String[] solution(String[] files) {
+        String[] answer = new String[files.length];
+
+        List<File> fileList = new ArrayList<>();
+        for (String file : files) {
+            char[] fileChars = file.toCharArray();
+            String head, number;
+            int numberStartIdx= 0, numberEndIdx = 0;
+
+            // 파싱
+            for (int i = 0; i < fileChars.length; i++) {
+                if (i != fileChars.length - 1) {
+                    if (!('0' <= fileChars[i] && fileChars[i] <= '9') && ('0' <= fileChars[i + 1] && fileChars[i + 1] <= '9')) { // 현재 문자는 숫자가 아니면서 다음 문자는 숫자라면
+                        numberStartIdx = i + 1;
+                    }
+                    if (('0' <= fileChars[i] && fileChars[i] <= '9') && !('0' <= fileChars[i + 1] && fileChars[i + 1] <= '9')) { // 현재 문자는 숫자이면서 다음 문자는  숫자가 아니라면
+                        numberEndIdx = i;
+                    }
+                }
+            }
+            head = new String(Arrays.copyOfRange(fileChars, 0, numberStartIdx));
+            number = new String(Arrays.copyOfRange(fileChars, numberStartIdx, numberEndIdx + 1));
+
+            fileList.add(new File(head.toUpperCase(), number, file));
+        }
+
+        // todo 정렬 조건 미완성
+        Collections.sort(fileList, (file1, file2) -> {
+            if (file1.getHead().equals(file2.getHead())) {
+                return file1.getNumber().compareTo(file2.getNumber());
+            }
+            return file1.getHead().compareTo(file2.getHead());
+        });
+
+        // 정답 배열에 담기
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = fileList.get(i).getOriginal();
+        }
+        return answer;
+    }
+
+    public static class File {
+        public String head;
+        public String number;
+        public String original;
+
+        public File(String head, String number, String original) {
+            this.head = head;
+            this.number = number;
+            this.original = original;
+        }
+
+        public String getHead() {
+            return head;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+        public String getOriginal() {
+            return original;
+        }
+    }
+}
---
+++ src/test/groovy/programmers/level2/Lesson17686Test.groovy
@@ -0,0 +1,29 @@
+package programmers.level2
+
+import spock.lang.Specification
+
+/**
+ *
+ * programmers.level2
+ * Lesson17686Test
+ *
+ * @author : USER
+ */
+class Lesson17686Test extends Specification {
+    def "[3차] 파일명 정렬"() {
+        when:
+        def solution = Lesson17686.solution(files as String[])
+
+        then:
+        solution == output as String[]
+
+        where:
+        files                                                                               | output
+        ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]        | ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
+        ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"] | ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
+    }
+
+    /*
+
+     */
+}
