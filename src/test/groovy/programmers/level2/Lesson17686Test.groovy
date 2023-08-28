53432a7d39ea5c87481d70957329b5e4d8e4f6d2
KoreanShortHair <korshorthaircat@gmail.com>
2023-08-28 12:18:02 +0900

    [프로그래머스] 파일명 정렬 (반례 케이스 추가)

--- src/test/groovy/programmers/level2/Lesson17686Test.groovy
+++ src/test/groovy/programmers/level2/Lesson17686Test.groovy
@@ -18,13 +18,14 @@ class Lesson17686Test extends Specification {
         solution == output as String[]
 
         where:
-        files                                                                               | output
-        ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]        | ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
-        ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"] | ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
-      // todo 반례 케이스 추가
+        files                                         | output
+//        ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]        | ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
+//        ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"] | ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
+//        ["muzi1.png", "MUZI1.png"]                                                          | ["muzi1.png", "MUZI1.png"]
+//        ["MUZI2.png", "muzi1.png", "MUZI01.png"]                                            | ["muzi1.png", "MUZI01.png", "MUZI2.png"]
+//        // 테일에도 숫자가 들어갈 수 있음 -> 파싱 로직 다시
+//        ["muzi1.png1", "MUZI1.png2", "MUZI1.png3", "muzi1.png4"]                            | ["muzi1.png1", "MUZI1.png2", "MUZI1.png3", "muzi1.png4"]
+//        ["img000012345", "img1.png", "img2", "IMG02"] | ["img000012345", "img1.png", "img2", "IMG02"]
+        ["img1.png", "img2", "IMG02"]                 | ["img1.png", "img2", "IMG02"]
     }
-
-    /*
-
-     */
 }
